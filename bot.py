1f34b823abe3e1bbf37ff8a951b09c9c9e31de1e
Alex <alexdonadio@Donadio.local>
2025-08-02 14:39:35 -0400

    Include !search in commands list

--- bot.py
+++ bot.py
@@ -332,7 +332,7 @@ async def ping(ctx):
 @bot.command(name='commands')
 async def list_commands(ctx):
     await ctx.send(
-        "📜 Commands: `!ping`, `!how`, `!submit <link>` or attach a file, `!vote <1-10>`, `!rank`, `!leaderboard`, `!postprompt`, `!chat <msg>`"
+        "📜 Commands: `!ping`, `!how`, `!submit <link>` or attach a file, `!vote <1-10>`, `!rank`, `!leaderboard`, `!postprompt`, `!chat <msg>`, `!search #tag`"
     )
 
 @bot.command(name='how')
